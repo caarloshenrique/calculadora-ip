e9b0e4b0dfdde3ee138739d66c5a96986a0b7fe9
caarloshenrique <carlos.silva@novaandradina.org>
2020-03-19 14:16:59 -0300

    Updates

---
+++ precache-manifest.9cf7cc126a67bb6d4bc1f352c70a3dc8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba20e5833d6eebdd251decb3c8e0f895",
+    "url": "/calculadora-ip/index.html"
+  },
+  {
+    "revision": "ed6db922dff7c0540065",
+    "url": "/calculadora-ip/static/js/2.b3bb87ca.chunk.js"
+  },
+  {
+    "revision": "3bec8b0652d89edd6036",
+    "url": "/calculadora-ip/static/js/main.3b1e35a8.chunk.js"
+  },
+  {
+    "revision": "fd3f397a2eebac4dbc39",
+    "url": "/calculadora-ip/static/js/runtime-main.705d5b76.js"
+  }
+]);
